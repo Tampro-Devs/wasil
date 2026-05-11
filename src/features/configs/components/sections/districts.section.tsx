ecd7156db19be7e394597064165efeb486b79e7b
sensaj <jafarsalum07@gmail.com>
2026-05-12 01:36:09 +0300

    Created District Section

---
+++ src/features/configs/components/sections/districts.section.tsx
@@ -0,0 +1,57 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableWrapper,
+} from "../../../../shared/components/table";
+import AppSelectField from "../../../../shared/components/app.select.field";
+import { regions } from "../../data";
+
+export default function DistrictsSection() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex gap-5">
+        <AppSelectField
+          placeholder="Select Region"
+          widthClass="w-60"
+          options={regions}
+        />
+      </div>
+
+      <TableWrapper
+        className="flex flex-col"
+        // error={{
+        //   title: "No Morogoro Districts Found",
+        //   message: "Click plus icon to add one",
+        // }}
+      >
+        <TableCaption>
+          <span className="font-bold me-1">Region:</span>
+          <span className="text-xs">Morogoro</span>
+        </TableCaption>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Wards</TableHead>
+              <TableHead>Members</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <TableRow key={index}>
+                <TableCell>Morogoro Mjini</TableCell>
+                <TableCell>12</TableCell>
+                <TableCell>30</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableWrapper>
+    </div>
+  );
+}
