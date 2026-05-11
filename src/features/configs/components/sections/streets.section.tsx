2f58cb27a4504fb6812d566b0bce4b1646e9db96
sensaj <jafarsalum07@gmail.com>
2026-05-12 01:36:41 +0300

    Created Streets Section

---
+++ src/features/configs/components/sections/streets.section.tsx
@@ -0,0 +1,65 @@
+import AppSelectField from "../../../../shared/components/app.select.field";
+import {
+  TableWrapper,
+  TableCaption,
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "../../../../shared/components/table";
+import { regions, districts, wards } from "../../data";
+
+export default function StreetsSection() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex gap-5">
+        <AppSelectField
+          placeholder="Select Region"
+          widthClass="w-60"
+          options={regions}
+        />
+        <AppSelectField
+          placeholder="Select Districts"
+          widthClass="w-60"
+          options={districts}
+        />
+        <AppSelectField
+          placeholder="Select Wards"
+          widthClass="w-60"
+          options={wards}
+        />
+      </div>
+
+      <TableWrapper
+        className="flex flex-col"
+        // error={{
+        //   title: "No Morogoro Districts Found",
+        //   message: "Click plus icon to add one",
+        // }}
+      >
+        <TableCaption>
+          <span className="font-bold me-1">Ward:</span>
+          <span className="text-xs">Ward 01</span>
+        </TableCaption>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Members</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <TableRow key={index}>
+                <TableCell>Mtaani</TableCell>
+                <TableCell>10</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableWrapper>
+    </div>
+  );
+}
