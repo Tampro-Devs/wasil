a5db947033c3b26980b2f019ab1ff9a26a77b0e7
sensaj <jafarsalum07@gmail.com>
2026-05-12 01:40:07 +0300

    Created OrganizationMainPage

---
+++ src/features/organization/pages/index.tsx
@@ -0,0 +1,6 @@
+import { setPageTitle } from "../../../utils/general_hooks";
+
+export default function OrganizationMainPage() {
+  setPageTitle("Organization");
+  return <div>OrganizationMainPage</div>;
+}
