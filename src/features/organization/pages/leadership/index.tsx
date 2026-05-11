67a8ad839ac422040ae3bca1533145f658136dda
sensaj <jafarsalum07@gmail.com>
2026-05-12 01:40:36 +0300

    Created LeadershipMainPage

---
+++ src/features/organization/pages/leadership/index.tsx
@@ -0,0 +1,6 @@
+import { setPageTitle } from "../../../../utils/general_hooks";
+
+export default function LeadershipMainPage() {
+  setPageTitle("Leadership");
+  return <div>LeadershipMainPage</div>;
+}
