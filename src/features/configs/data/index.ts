551c79989a0f097b06c83bfb54f87d02c4624485
sensaj <jafarsalum07@gmail.com>
2026-05-12 01:37:01 +0300

    Created dummy data

---
+++ src/features/configs/data/index.ts
@@ -0,0 +1,20 @@
+import type { SelectOption } from "../../../shared/components/app.select.field";
+
+export const regions: SelectOption[] = [
+  { label: "Morogoro", value: "122212" },
+  { label: "Dar es salaam", value: "122212" },
+];
+
+export const districts: SelectOption[] = [
+  { label: "Morogoro Mjini", value: "122212" },
+  { label: "Mvomero", value: "122212" },
+  { label: "Turiani", value: "122212" },
+  { label: "Kilosa", value: "122212" },
+];
+
+export const wards: SelectOption[] = [
+  { label: "Ward 01", value: "122212" },
+  { label: "Ward 02", value: "122212" },
+  { label: "Ward 03", value: "122212" },
+  { label: "Ward 04", value: "122212" },
+];
