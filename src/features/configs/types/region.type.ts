ae8f4337c58f4986935e3033931bc2de2b8d43b4
sensaj <jafarsalum07@gmail.com>
2026-05-18 06:01:05 +0300

    Created Region interface

---
+++ src/features/configs/types/region.type.ts
@@ -0,0 +1,38 @@
+export interface Region {
+  regionId: string;
+  name: string;
+}
+
+export const regionDummies: Region[] = [
+  { regionId: "TZ-01", name: "Dodoma" },
+  { regionId: "TZ-02", name: "Arusha" },
+  { regionId: "TZ-03", name: "Kilimanjaro" },
+  { regionId: "TZ-04", name: "Tanga" },
+  { regionId: "TZ-05", name: "Morogoro" },
+  { regionId: "TZ-06", name: "Pwani" },
+  { regionId: "TZ-07", name: "Dar es Salaam" },
+  { regionId: "TZ-08", name: "Lindi" },
+  { regionId: "TZ-09", name: "Mtwara" },
+  { regionId: "TZ-10", name: "Ruvuma" },
+  { regionId: "TZ-11", name: "Iringa" },
+  { regionId: "TZ-12", name: "Mbeya" },
+  { regionId: "TZ-13", name: "Singida" },
+  { regionId: "TZ-14", name: "Tabora" },
+  { regionId: "TZ-15", name: "Rukwa" },
+  { regionId: "TZ-16", name: "Kigoma" },
+  { regionId: "TZ-17", name: "Shinyanga" },
+  { regionId: "TZ-18", name: "Kagera" },
+  { regionId: "TZ-19", name: "Mwanza" },
+  { regionId: "TZ-20", name: "Mara" },
+  { regionId: "TZ-21", name: "Manyara" },
+  { regionId: "TZ-22", name: "Njombe" },
+  { regionId: "TZ-23", name: "Katavi" },
+  { regionId: "TZ-24", name: "Simiyu" },
+  { regionId: "TZ-25", name: "Geita" },
+  { regionId: "TZ-26", name: "Songwe" },
+  { regionId: "TZ-27", name: "Kaskazini Unguja" },
+  { regionId: "TZ-28", name: "Kusini Unguja" },
+  { regionId: "TZ-29", name: "Mjini Magharibi" },
+  { regionId: "TZ-30", name: "Kaskazini Pemba" },
+  { regionId: "TZ-31", name: "Kusini Pemba" },
+];
