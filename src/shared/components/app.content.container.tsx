0f7e8e7556ba7bd47c43df0da671d63fd4be93a7
sensaj <jafarsalum07@gmail.com>
2026-05-18 06:04:37 +0300

    Added AppContentHeader

--- src/shared/components/app.content.container.tsx
+++ src/shared/components/app.content.container.tsx
@@ -1,29 +1,54 @@
 import React from "react";
 import { cn } from "../../utils/cn";
 
-export default function AppContentContainer({
-  title,
-  actions,
-  children,
-  className,
-}: {
-  title?: string;
-  actions?: React.ReactNode;
+interface AppContentProps extends React.ComponentProps<"div"> {
   className?: string;
-  children: React.ReactNode;
-}) {
+}
+
+export function AppContentContainer({ className, ...props }: AppContentProps) {
   return (
     <div
       className={cn(
         "border-r p-3 rounded-lg border border-slate-300",
         className,
       )}
-    >
-      <div className={`${title || actions ? "py-2 flex justify-between" : ""}`}>
-        {title && <span>{title}</span>}
-        {actions}
-      </div>
-      {children}
+      {...props}
+    />
+  );
+}
+
+interface AppContentHeaderProps extends React.ComponentProps<"div"> {
+  title?: string;
+  actions?: React.ReactNode;
+  children?: React.ReactNode;
+}
+
+export function AppContentHeader({
+  title,
+  actions,
+  className,
+  children,
+  ...props
+}: AppContentHeaderProps) {
+  return (
+    <div className={cn("py-2 flex justify-between", className)} {...props}>
+      {children ? (
+        children
+      ) : (
+        <>
+          <div className="flex flex-col">
+            {title && <span className="font-bold">{title}</span>}
+          </div>
+          <span>{actions}</span>
+        </>
+      )}
     </div>
   );
+}
+
+export function AppContentBody({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return <div className={cn("flex flex-col", className)} {...props} />;
 }
