3e3e6b596825684b36374c64c49f46804098a264
sensaj <jafarsalum07@gmail.com>
2026-05-18 05:56:13 +0300

    Added regions select option dummies

--- src/features/configs/data/index.ts
+++ src/features/configs/data/index.ts
@@ -1,8 +1,37 @@
 import type { SelectOption } from "../../../shared/components/app.form.fields";
 
 export const regions: SelectOption[] = [
-  { label: "Morogoro", value: "122212" },
-  { label: "Dar es salaam", value: "122212" },
+  { value: "TZ-01", label: "Dodoma" },
+  { value: "TZ-02", label: "Arusha" },
+  { value: "TZ-03", label: "Kilimanjaro" },
+  { value: "TZ-04", label: "Tanga" },
+  { value: "TZ-05", label: "Morogoro" },
+  { value: "TZ-06", label: "Pwani" },
+  { value: "TZ-07", label: "Dar es Salaam" },
+  { value: "TZ-08", label: "Lindi" },
+  { value: "TZ-09", label: "Mtwara" },
+  { value: "TZ-10", label: "Ruvuma" },
+  { value: "TZ-11", label: "Iringa" },
+  { value: "TZ-12", label: "Mbeya" },
+  { value: "TZ-13", label: "Singida" },
+  { value: "TZ-14", label: "Tabora" },
+  { value: "TZ-15", label: "Rukwa" },
+  { value: "TZ-16", label: "Kigoma" },
+  { value: "TZ-17", label: "Shinyanga" },
+  { value: "TZ-18", label: "Kagera" },
+  { value: "TZ-19", label: "Mwanza" },
+  { value: "TZ-20", label: "Mara" },
+  { value: "TZ-21", label: "Manyara" },
+  { value: "TZ-22", label: "Njombe" },
+  { value: "TZ-23", label: "Katavi" },
+  { value: "TZ-24", label: "Simiyu" },
+  { value: "TZ-25", label: "Geita" },
+  { value: "TZ-26", label: "Songwe" },
+  { value: "TZ-27", label: "Kaskazini Unguja" },
+  { value: "TZ-28", label: "Kusini Unguja" },
+  { value: "TZ-29", label: "Mjini Magharibi" },
+  { value: "TZ-30", label: "Kaskazini Pemba" },
+  { value: "TZ-31", label: "Kusini Pemba" },
 ];
 
 export const districts: SelectOption[] = [
@@ -17,4 +46,10 @@ export const wards: SelectOption[] = [
   { label: "Ward 02", value: "122212" },
   { label: "Ward 03", value: "122212" },
   { label: "Ward 04", value: "122212" },
+];
+export const streets: SelectOption[] = [
+  { label: "Street 01", value: "122212" },
+  { label: "Street 02", value: "122212" },
+  { label: "Street 03", value: "122212" },
+  { label: "Street 04", value: "122212" },
 ];
