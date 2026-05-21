512bfbd813e5bf0ec759508a5b1604153f15ad40
sensaj <jafarsalum07@gmail.com>
2026-05-21 19:27:08 +0300

    Added action component

--- src/shared/types/app.error.ts
+++ src/shared/types/app.error.ts
@@ -1,7 +1,9 @@
 import type { LucideIcon } from "lucide-react";
+import type React from "react";
 
 export type AppError = {
   title?: string;
   message?: string;
   Icon?: LucideIcon;
+  Action?: React.ReactNode;
 };
