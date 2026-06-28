b7461c1efb18979ad3c6426e6e87b74794334839
sensaj <jafarsalum07@gmail.com>
2026-06-28 22:49:32 +0300

    Added env configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import envCompatible from "vite-plugin-env-compatible";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    envCompatible(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
   base: "/wasil",
