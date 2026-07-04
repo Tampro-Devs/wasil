ce6a1a1cdf48fa04702d9dc1d2a58e5e619acb69
sensaj <jafarsalum07@gmail.com>
2026-07-04 07:47:49 +0300

    Started to use query Client

--- src/api.service.config/query.config/index.ts
+++ src/api.service.config/query.config/index.ts
@@ -1,3 +1,15 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient({});
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
