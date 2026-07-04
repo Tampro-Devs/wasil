9bd4d5ffd90b86dc87173c6a54c99f7723bbee0b
sensaj <jafarsalum07@gmail.com>
2026-07-04 07:51:35 +0300

    feat: Added App Spinner

---
+++ src/shared/components/loading.indicators.tsx
@@ -0,0 +1,12 @@
+import { SpinnerCircular } from "spinners-react";
+
+export default function AppSpinner() {
+  return (
+    <SpinnerCircular
+      size={30}
+      color="#3e3c92"
+      secondaryColor="#dfdee8"
+      speed={1000}
+    />
+  );
+}
