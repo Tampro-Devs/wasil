1f35905ae38c3c345c50432ec46f034742de2539
sensaj <jafarsalum07@gmail.com>
2026-07-18 07:48:51 +0300

    feat: Started to use BrowserRouter instead of HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import WasilApp from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./api.service.config/query.config/index.ts";
 import { Provider } from "react-redux";
 import { store } from "./shared/store/index.ts";
+import { AuthProvider } from "./modules/auth/context/auth.context.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // {/* <BrowserRouter> */}
-  <HashRouter>
+  <BrowserRouter>
     <StrictMode>
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
-          <WasilApp />
+          <AuthProvider>
+            <WasilApp />
+          </AuthProvider>
         </QueryClientProvider>
       </Provider>
     </StrictMode>
-  </HashRouter>,
-  // {/* </BrowserRouter> */}
+  </BrowserRouter>,
 );
